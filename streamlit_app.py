a853d94fd58b22f7230c45546e85582fea84d3d5
Jeff Ho <95098360+JHO-ds@users.noreply.github.com>
2023-05-20 16:02:29 +0800

    Update streamlit_app.py
    Fixed bug to print the url

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit_doc_url = r"https://docs.streamlit.io/library/api-reference"
 
 streamlit.title("First Streamlit App")
-streamlit.write(f"Refer here for Streamlit Documentation - {}")
+streamlit.write(f"Refer here for Streamlit Documentation - {streamlit_doc_url}")
