0d08df3f0f231b212ee147594764fe6938ab7906
Jeff Ho <95098360+JHO-ds@users.noreply.github.com>
2023-05-20 16:11:57 +0800

    Update streamlit_app.py
    Updated script according to course requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit
+import pandas as pd
 
-streamlit_doc_url = r"https://docs.streamlit.io/library/api-reference"
+my_fruit_list_s3_url = r"https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+my_fruit_list = pd.read_csv(my_fruit_list_s3_url)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-streamlit.title("First Streamlit App")
-streamlit.write(f"Refer here for Streamlit Documentation - {streamlit_doc_url}")
+streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Stawberries"])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
