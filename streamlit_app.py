0e4e2d07340f3fdcf0db311fdadbd58e4ede20f5
Jeff Ho <95098360+JHO-ds@users.noreply.github.com>
2023-05-20 16:01:55 +0800

    Update streamlit_app.py
    Imported Streamlit library
    Added title and documentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
+import streamlit
 
+streamlit_doc_url = r"https://docs.streamlit.io/library/api-reference"
+
+streamlit.title("First Streamlit App")
+streamlit.write(f"Refer here for Streamlit Documentation - {}")
