5a9690fe9e7f71a3eebd669bef82b5cd204584c0
Jeff Ho <95098360+JHO-ds@users.noreply.github.com>
2023-05-21 15:56:44 +0800

    Update streamlit_app.py
    Import snowflake connector python package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas as pd
 import requests
+import snowflake.connector
 
 my_fruit_list_s3_url = r"https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 my_fruit_list = pd.read_csv(my_fruit_list_s3_url)
