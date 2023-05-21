17926afa87458022aae2a7141b6b87cc0be46010
Jeff Ho <95098360+JHO-ds@users.noreply.github.com>
2023-05-21 15:23:30 +0800

    Update streamlit_app.py
    Added REST API (GET Request) to get fruit information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 my_fruit_list_s3_url = r"https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
 my_fruit_list = pd.read_csv(my_fruit_list_s3_url)
@@ -17,3 +18,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), default=['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_query = streamlit.write('The user entered ', fruit_choice, default="apple")
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_query}")
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
