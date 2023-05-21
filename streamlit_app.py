6419e9903927a1c848d1a7dd59648e6d2109134e
Jeff Ho <95098360+JHO-ds@users.noreply.github.com>
2023-05-21 16:36:50 +0800

    Update streamlit_app.py
    Fixed typo to close function

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,5 +73,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   insert_status = insert_row_snowflake(add_my_fruit)
-  my_cnx.cloase()
+  my_cnx.close()
   streamlist.text(insert_status)
